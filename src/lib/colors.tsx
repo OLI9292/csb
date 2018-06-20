51851af8f2509fe17194022a9c69b0d459f82a9a
Oliver Plunkett <otplunkett@gmail.com>
2018-06-20 15:45:23 -0400

    nicer password entry ux

--- src/lib/colors.tsx
+++ src/lib/colors.tsx
@@ -8,7 +8,8 @@ export const colors = {
   red10l: '#FF7171',
   gray: '#828282',
   lightGray: '#d9d9d9',
-  white: '#ffffff'
+  white: '#ffffff',
+  transparent: 'transparent'
 };
 
 export const lighten10l = function(color: string):string  {
@@ -17,6 +18,7 @@ export const lighten10l = function(color: string):string  {
     case colors.red: return colors.red10l;
     case colors.green: return colors.green10l;
     case colors.white: return colors.white;
+    case colors.transparent: return colors.transparent;
     default: return "black"
   }
 };
